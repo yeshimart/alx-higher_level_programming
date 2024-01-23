00d2a70c5e1b50169d6801dad3e657944b8c456d
yeshimart <yeshitla12345@gmail.com>
2024-01-23 10:46:35 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
+
 """Defines a square with private instance attribute and area method."""
 
 
 class Square:
-    """Square class with private instance attribute and area method."""
-    
-    def __init__(self, size=0):
-        """Initializes a new instance of the Square class.
-        
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
+    """Represent a square."""
 
-    def area(self):
-        """Calculates the area of the square.
-        
-        Returns:
-            int: The area of the square.
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
         """
-        return self.__size ** 2
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
